284f8f5cf737f390876521a716792a5be2561ec4
olis1996 <oli@datacater.io>
2023-01-19 13:12:34 +0100

    solved exercise 10

---
+++ uebung10/aufgabe1.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+    int vectorDimensions;
+    int* firstVec;
+    int* secondVec;
+
+    printf("Please type in the vectors' dimension: ");
+    scanf(" %d", &vectorDimensions);
+
+    firstVec = (int*) malloc(vectorDimensions * sizeof(int));
+    secondVec = (int*) malloc(vectorDimensions * sizeof(int));
+
+    double currentInput;
+
+    printf("Please type in the first vector's components: \n");
+    for (int x = 0; x < vectorDimensions; x++) {
+        printf("vector component %d: ", x + 1);
+        scanf(" %lf", &currentInput);
+        firstVec[x] = currentInput;
+    }
+
+    printf("Please type in the second vector's components: \n");
+    for (int x = 0; x < vectorDimensions; x++) {
+        printf("vector component %d: ", x + 1);
+        scanf(" %lf", &currentInput);
+        secondVec[x] = currentInput;
+    }
+
+    double sum = 0;
+
+    for (int x = 0; x < vectorDimensions; x++) {
+        sum += firstVec[x] * secondVec[x];
+    }
+
+    printf("The vectors' dot product is: %lf\n", sum);
+
+    free(firstVec);
+    free(secondVec);
+}
---
+++ uebung10/aufgabe2.c
@@ -0,0 +1,83 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+int get_int_from_input(int groesserNull) {
+    int result;
+    int number;
+
+    while (1) {
+        result = scanf("%d", &number);
+        
+        // necessary for whatever reason
+        getchar();
+
+        if (result != 1) {
+            printf("Ihre Eingabe ist keine Zahl.\n");
+            printf("Bitte geben sie eine Zahl ein: ");
+            continue;
+        }
+
+        if (groesserNull && number <= 0) {
+            printf("Die Zahl muss > 0 sein.\n");
+            printf("Bitte geben sie eine Zahl ein: ");
+            continue;
+        }
+
+        return number;
+    }
+}
+
+int *folge_einlesen(int laenge) {
+    int * numberArray = malloc(laenge * sizeof(int));
+
+    if (numberArray == NULL) {
+        printf("Array initialisierung fehlgeschlagen.");
+        exit(1);
+    }
+    
+    for (int i = 0; i < laenge; i++) {
+        printf("Bitte geben Sie die %d. Zahl ein: ", i + 1);
+        numberArray[i] = get_int_from_input(0);
+    }
+
+    return numberArray;
+}
+
+int main() {
+    int laenge;
+    int gesamtLaenge;
+
+    printf("Laenge der einzugebenden Zahlenfolgen eingeben: ");
+    laenge = get_int_from_input(1);
+    gesamtLaenge = 2 * laenge;
+
+    printf("Bitte geben Sie die Zahlen der ersten Folge ein: \n");
+    int * erstesArray = folge_einlesen(laenge);
+
+    printf("Bitte geben Sie die Zahlen der zweiten Folge ein: \n");
+    int * zweitesArray = folge_einlesen(laenge);
+
+    int * gesamtArray = malloc(gesamtLaenge * sizeof(int));
+    int gesamtArrayCounter = 0;
+
+    for (int i = 0; i < laenge; i++) {
+        gesamtArray[gesamtArrayCounter] = erstesArray[i];
+        gesamtArrayCounter++;
+        
+        gesamtArray[gesamtArrayCounter] = zweitesArray[i];
+        gesamtArrayCounter++;
+    }
+
+    printf("Gemischtes Array ist:");
+    for (int i = 0; i < gesamtLaenge; i++) {
+        printf(" %d", gesamtArray[i]);
+    }
+    printf("\n");
+
+    free(erstesArray);
+    free(zweitesArray);
+    free(gesamtArray);
+
+    return 0;
+}
